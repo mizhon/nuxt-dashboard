c25a47354469520378ab03e9f83d0a1b328099d2
mizhon <mizhon@live.cn>
2021-05-19 00:02:45 +0800

    update codes

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="container">page-1</div>
+  <div class="tabs-group">
+    <section v-for="(tab, idx) in tabs" :key="idx">
+      <div class="tab-warpper">
+        <div class="tab">{{ tab.name }}</div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
+  data() {
+    return {
+      tabs: [
+        {
+          name: 'Meetings',
+        },
+        {
+          name: 'Messages',
+        },
+      ],
+    }
+  },
 }
 </script>
-
-<style>
-.container {
+<style lang="scss">
+.tabs-group {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -18,5 +35,9 @@ export default {
   background-color: #f6f7f9;
   width: 100%;
   height: 100%;
+  .tab-wrapper {
+    border: 1px solid #efefef;
+    padding: 20px;
+  }
 }
 </style>
